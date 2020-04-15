360257a6ba78f9e982d45f1e025436f7d916b560
Mikko Harju <mikko@taiste.fi>
2020-04-15 19:48:18 +0300

    Started implementing a better view split and control mechanism

--- src/lab/views.cljs
+++ src/lab/views.cljs
@@ -2,11 +2,100 @@
   (:require-macros [hiccups.core :as hiccups :refer [html]])
   (:require [hiccups.runtime]
             [clojure.set]
+            [clojure.string :as str]
             [lab.layout :as layout]
             ["jquery" :as $]))
 
 (defonce views (atom {}))
 (defonce components (atom {}))
+
+(defn find-end [start size defs]
+  (when (<= 0 (dec start) (dec (count defs)))
+    (let [end (:result (reduce (fn [{:keys [cumulative index] :as acc} cumulative']
+                                (if (>= (+ cumulative cumulative') size)
+                                  (reduced {:result index})
+                                  (-> acc
+                                      (update :cumulative + cumulative')
+                                      (update :index inc))))
+                              {:cumulative 0 :index 1}
+                              (subvec defs (dec start))))]
+      (when end
+        (+ start end)))))
+
+(defn split [id after def-key {views :views :as data}]
+  (let [defs (get data def-key)
+        index (->> views (map-indexed (fn [i {id' :id}] (when (= after id') i))) (remove nil?) first)
+        {[size-col size-row] :size [start-col start-row] :start :as sibling} (nth views index)
+        col-index (dec (if (= :row-defs def-key) start-row start-col))
+        original-size (if (= :row-defs def-key) size-row size-col)
+        new-size (/ original-size 2)
+        new-size-def (if (= :row-defs def-key)
+                       [size-col new-size]
+                       [new-size size-row])
+        need-new-def? (let [end-old (dec (find-end (inc col-index) original-size defs))]
+                        (if-let [usable-columns (when (and (<= (+ col-index end-old) (count defs))
+                                                           (<= 2 (- end-old col-index)))
+                                                   (subvec defs col-index end-old))]
+                          (let [a (find-end 1 new-size usable-columns)
+                                b (find-end a new-size usable-columns)]
+                            (not (and a b)))
+                          true))
+        col-def (nth defs col-index)
+        new-col-def  (/ col-def 2)
+        col-defs (if need-new-def?
+                   (->> (concat (subvec defs 0 col-index)
+                           [new-col-def new-col-def]
+                           (subvec defs (inc col-index)))
+                        (into []))
+                   defs)
+        move-if-needed (partial mapv (fn [{[vc vr] :start :as v}]
+                                       (assoc v :start (if (= :row-defs def-key)
+                                                         [vc (if (< start-row vr) (inc vr) vr)]
+                                                         [(if (< start-col vc) (inc vc) vc) vr]))))
+        views (->> (concat (subvec views 0 index)
+                     [(assoc sibling :size new-size-def)
+                      {:id id
+                       :start (if (= :row-defs def-key)
+                                [start-col (find-end (inc col-index) new-size defs)]
+                                [(find-end (inc col-index) new-size defs) start-row])
+                       :size new-size-def}]
+                     (cond->> (subvec views (inc index))
+                       need-new-def?
+                       move-if-needed))
+                   (into []))]
+    (assoc data def-key col-defs
+                :views views)))
+
+(defn ->css [{:keys [views col-defs row-defs]}]
+  (str
+    (str ".container { grid-template: "
+         (->> row-defs (map #(str % "%")) (str/join " "))
+         " / "
+         (->> col-defs (map #(str % "vw")) (str/join " "))
+         "; }\n")
+    (->>
+      views
+      (map (fn [{[start-col start-row] :start [size-col size-row] :size id :id}]
+             (let [end-col (find-end start-col size-col col-defs)
+                   end-row (find-end start-row size-row row-defs)]
+               (str "#" (name id) " { grid-area: " start-row " / " start-col " / " end-row " / " end-col "; }"))))
+      (str/join "\n"))))
+
+(comment
+  (find-end 1 100 [50 25 25])
+  (->>
+    {:views [{:id :view1 :start [1 1] :size [100 100]}]
+     :col-defs [100]
+     :row-defs [100]}
+    (split :view2 :view1 :col-defs)
+    (split :view3 :view1 :row-defs)
+    (split :view4 :view3 :col-defs)
+    (split :view5 :view3 :col-defs)
+    (split :view6 :view5 :row-defs)
+    (split :view7 :view1 :col-defs)
+    (split :view8 :view2 :row-defs)
+    ->css
+    println))
 
 (defn add-view!
   ([id] (add-view! (js/document.querySelector "#dashboard") id))
